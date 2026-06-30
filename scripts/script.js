5b54701b5ca9a97fa25c95feeb7fa19f33a4ba44
Liam Elliot <liamelliotllk@gmail.com>
2026-06-29 21:08:54 -0300

    feat: Adiciona mensagem de boas-vindas ao carregar página

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
-"Arquivo script.js" 
+//arquivo script
 function mostrarMensagem() {
     document.getElementById("mensagem").innerHTML = 
         "Parabéns! Seu projeto está funcionando corretamente.";
 }
+// Mensagem de boas vindas
+window.onload = function() {
+    alert("Bem-vindo ao Site Full Stack Noite");
+};
